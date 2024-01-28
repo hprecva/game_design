851a6edefd1747eaff18639840771c9e92419b11
hprecva <hprecva@gmail.com>
2024-01-27 23:24:04 -0600

    Primer commit, Inicio del proyecto

---
+++ Assets/hola_mundo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hola_mundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("¡Hola Mundo!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

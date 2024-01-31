f5238ee49ce8449f663e6178335bd4970758182d
hprecva <hprecva@gmail.com>
2024-01-30 22:43:25 -0600

    Segundo commit, Adición assets, Primer ventana

---
+++ Assets/links.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class links : MonoBehaviour
+{
+    public void one() {
+        Application.OpenURL ("https://www.febucci.com/2018/10/unity-ui-tutorial/");
+    }
+
+    public void two() {
+        Application.OpenURL ("https://www.udgvirtual.udg.mx/");
+    }
+
+    public void three() {
+        Application.OpenURL ("https://www.youtube.com/");
+    }
+
+    public void four() {
+        Application.OpenURL ("https://github.com/hprecva/game_design");
+    }
+
+    public void five() {
+        Application.OpenURL ("https://www.facebook.com/?locale=es_LA");
+    }
+
+    public void six() {
+        Application.OpenURL ("https://kenney.nl/");
+    }
+}
